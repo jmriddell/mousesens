f539746952a2bc316675adeaf93323e62d11e8b2
Juan Molina <j.molina.riddell@gmail.com>
2020-12-15 12:20:36 -0500

    move xinput related funtions to separate file

--- mousesens/mousesens.py
+++ mousesens/mousesens.py
@@ -2,50 +2,7 @@
 
 import click
 import subprocess
-
-
-def get_name(line):
-    """Get name of a pointer device given it's line from 'xinput --list'."""
-    return line[6:].split("\t", maxsplit=1)[0].rstrip()
-
-
-def get_pointer_devices():
-    """Get a list of pointer device names using 'xinput --list'."""
-    string = subprocess.run(
-        ["xinput", "--list"],
-        capture_output=True,
-        encoding="utf8"
-    ).stdout
-
-    return list(
-        map(
-            get_name,
-            filter(
-                lambda x: x.startswith('⎜'),
-                string.splitlines()
-            )
-        )
-    )
-
-
-def mousesens_command(device, sensibility):
-    """Make the command to be executed to change the pointer sensibility."""
-    sens_string = format(sensibility, 'f')
-    return [
-        'xinput',
-        '--set-prop',
-        device,
-        'Coordinate Transformation Matrix',
-        sens_string + ',',
-        '0.000000,',
-        '0.000000,',
-        '0.000000,',
-        sens_string + ',',
-        '0.000000,',
-        '0.000000,',
-        '0.000000,',
-        '1.000000'
-    ]
+import mousesens.xinput_interface as xi
 
 
 @click.group()
@@ -56,18 +13,18 @@ def cli():
 
 @click.command(name="set")
 @click.argument(
-    "device", type=click.Choice(get_pointer_devices())
+    "device", type=click.Choice(xi.get_pointer_devices())
 )
 @click.argument("sensibility", type=click.FLOAT)
 def set_sensibility(device, sensibility):
     """Set the sensibility of a pointer device."""
-    subprocess.run(mousesens_command(device, float(sensibility)),)
+    xi.set_sensibility(device, float(sensibility))
 
 
 @click.command(name="list")
 def list_devices():
     """List available pointer devices."""
-    devices = get_pointer_devices()
+    devices = xi.get_pointer_devices()
 
     for device in devices:
         click.echo(message=device)
---
+++ mousesens/xinput_interface.py
@@ -0,0 +1,51 @@
+"""Interface with xinput functionality."""
+
+import subprocess
+
+
+def get_name(line):
+    """Get name of a pointer device given it's line from 'xinput --list'."""
+    return line[6:].split("\t", maxsplit=1)[0].rstrip()
+
+
+def get_pointer_devices():
+    """Get a list of pointer device names using 'xinput --list'."""
+    string = subprocess.run(
+        ["xinput", "--list"],
+        capture_output=True,
+        encoding="utf8"
+    ).stdout
+
+    return list(
+        map(
+            get_name,
+            filter(
+                lambda x: x.startswith('⎜'),
+                string.splitlines()
+            )
+        )
+    )
+
+
+def mousesens_command(device:str, sensibility:float):
+    """Make the command to be executed to change the pointer sensibility."""
+    sens_string = format(sensibility, 'f')
+    return [
+        'xinput',
+        '--set-prop',
+        device,
+        'Coordinate Transformation Matrix',
+        sens_string + ',',
+        '0.000000,',
+        '0.000000,',
+        '0.000000,',
+        sens_string + ',',
+        '0.000000,',
+        '0.000000,',
+        '0.000000,',
+        '1.000000'
+    ]
+
+def set_sensibility(device:str, sensibility:float):
+    """Set the sensibility of a pointer device."""
+    subprocess.run(mousesens_command(device, sensibility),)
